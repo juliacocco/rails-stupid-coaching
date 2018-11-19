48421996bd7f2f4a2e22c6c0d5fcd12fd78681f2
Julia Cocco <juliatcocco@gmail.com>
2018-11-19 17:25:54 +0100

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,16 +2,14 @@ class QuestionsController < ApplicationController
   def ask
   end
 
-def answer
-  question = params[:questions]
-    if question == "i am going to work right now!"
-    @answer = "great bye bye"
-    elsif question
-    @answer = "Silly question, get dressed and go to work!"
+  def answer
+    @question = params[:question]
+    if @question == "i am going to work right now!"
+      @answer = "great bye bye"
+    elsif @question[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
     else
-    @answer = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
-
-
